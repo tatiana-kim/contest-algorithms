22a0c80acc0e330a90f02bced56274b778cd70b7
berthakim <kte.ent@gmail.com>
2021-06-16 18:56:19 +0900

    Slow but more detailed code

--- contest4/F_vents.py
+++ contest4/F_vents.py
@@ -1,14 +1,42 @@
 """
 F_vents.py
 """
-
-f = open("F/input3.txt", "r")
+# read data
+f = open("F/input.txt", "r")
 lines = f.readlines()
 
 f.close()
 
 lst = []
 for line in lines:
-	lst.append(line.strip("\n").split())
+    lst.append(line.strip("\n").split())
 
-print(lst[-1])
+if not lst[-1]:
+    lst[:] = lst[:-1]
+
+# create a dictionnary and write the data
+d = {}
+for i in lst:
+    if i[0] not in d:
+        d[i[0]] = {i[1]: int(i[2])}
+    elif i[1] not in d[i[0]]:
+        d[i[0]][i[1]] = int(i[2])
+    else:  # i[1] in d[i[0]]
+        d[i[0]][i[1]] += int(i[2])
+
+# print the answer
+for name, value in sorted(d.items()):
+    print(f'{name}:')
+    for product, amount in sorted(value.items()):
+        print(product, amount)
+
+
+"""
+Ivanov:
+envelope 5
+marker 3
+paper 17
+Petrov:
+envelope 20
+pens 5
+"""
