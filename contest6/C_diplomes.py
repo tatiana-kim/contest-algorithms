01553dac2489fd69f150fbe1989c281ac216ca48
berthakim <kte.ent@gmail.com>
2021-06-20 02:39:06 +0900

    Added tests

--- contest6/C_diplomes.py
+++ contest6/C_diplomes.py
@@ -12,7 +12,23 @@ def checkboard(xlength, params):
     return (xlength // width) * (xlength // height) >= number
 
 
-w, h, n = list(map(int, input().split()))
-left = 0  # or left = w
-right = max(w * n, h * n)
-print(left_bin_search(left, right, checkboard, (w, h, n)))
+def main():
+    w, h, n = list(map(int, input().split()))
+    left = 0  # or left = w
+    right = max(w * n, h * n)
+    print(left_bin_search(left, right, checkboard, (w, h, n)))
+
+
+def test(algo):
+    w, h, n = 2, 3, 10
+    right = max(w * n, h * n)
+    assert algo(0, right, checkboard, (w, h, n)) == 9, "WA :("
+    print("Test 1: Ok")
+
+
+if __name__ == "__main__":
+	main()
+
+
+# if You want to launch the test,
+# replace "main()" by "test(left_bin_search)" in the last line
