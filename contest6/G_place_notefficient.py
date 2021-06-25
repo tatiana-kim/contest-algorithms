ec4d46125174f56405a7fe10b44ab1d67c20c80b
berthakim <kte.ent@gmail.com>
2021-06-25 12:59:03 +0900

    Removed commented-out comments

--- contest6/G_place_notefficient.py
+++ contest6/G_place_notefficient.py
@@ -1,4 +1,5 @@
 # that algorithm works but take a very long time. So not efficient
+
 def checkwalkway(tothrow, params):
     n, m, t = params
     k = tothrow // 2 + 1
@@ -15,14 +16,6 @@ def checkwalkway(tothrow, params):
 
 def main():
     n, m, t = [int(input()) for i in range(3)]
-    # n, m, t = 6, 7, 38  # 2
-    # n, m, t = 5, 20, 46  # 1
-    # n, m, t = 20, 10, 144  # 3
-    # n, m, t = 50, 50, 899  # 4
-    # n, m, t = 50, 50, 900  # 5 (not here?)
-    # # test 17 takes a veeery long time:
-    # n, m, t = 1600000000, 1450000000, 2310000003500000805  # answer: 700000007 (test 17)
-    # n, m, t = 500, 1000, 100  # answer: 0 (test 13)
     l = n * m - t
     r = n * m
     for noused in range(l, r):
