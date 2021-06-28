74b88fe6556bf9eb645554ce46784e146d98c360
berthakim <kte.ent@gmail.com>
2021-06-29 07:56:06 +0900

    Frst commit

---
+++ contest2/D.py
@@ -0,0 +1,28 @@
+"""
+Дан список чисел. Определите, сколько в этом списке элементов, которые
+больше двух своих соседей и выведите количество таких элементов.
+
+Формат ввода
+Вводится список чисел. Все числа списка находятся на одной строке.
+
+Формат вывода
+Выведите ответ на задачу.
+"""
+
+def greater_than_neighb(seq):
+    n = len(seq)
+    count = 0
+    for i in range(1, n-1):
+        if seq[i] > seq[i-1] and seq[i] > seq[i+1]:
+        # if seq[i-1] < seq[i] > seq[i+1]:
+            count += 1
+    return count
+
+
+def main():
+    seq1 = list(map(int, input().split()))
+    print(greater_than_neighb(seq1))
+
+
+if __name__ == "__main__":
+    main()
