d060e8a2643d5e457dbc91f95fea73d221c34198
berthakim <kte.ent@gmail.com>
2021-07-01 14:06:34 +0900

    Added tests

--- contest2/H_max_product_of3.py
+++ contest2/H_max_product_of3.py
@@ -36,11 +36,34 @@ def find_maxproduct_of3(a):
     return max1, max2, max3
 
 
+def tests(algo):
+    assert find_maxproduct_of3([3, 5, 1, 7, 9, 0, 9, -3, 10]) == (10, 9, 9), "WA :("
+    assert find_maxproduct_of3([-5, -30000, -12]) == [-5, -30000, -12], "WA :("
+    assert find_maxproduct_of3([1, 2, 3]) == [1, 2, 3], "WA :("
+    assert find_maxproduct_of3([-2, 7, 10, 12]) == (7, 10, 12), "WA :("
+
+    f = open("H/input10.txt", "r")
+    seq = [int(i) for i in next(f).strip().split()]
+    f.close()
+    assert find_maxproduct_of3(seq) == (-54, -52, 55), "WA :("
+
+    f = open("H/input13.txt", "r")
+    seq = [int(i) for i in next(f).strip().split()]
+    f.close()
+    assert find_maxproduct_of3(seq) == (-99, -98, 43), "WA :("
+
+    f = open("H/input16.txt", "r")
+    seq = [int(i) for i in next(f).strip().split()]
+    f.close()
+    assert find_maxproduct_of3(seq) == (-4, -4, -4), "WA :("
+
+
 def main():
     seq = list(map(int, input().split()))
-    
-    print(*find_maxproduct_of3(seq))
+    print(find_maxproduct_of3(seq))
 
 
 if __name__ == "__main__":
     main()
+
+# to launch tests: tests(find_maxproduct_of3)
