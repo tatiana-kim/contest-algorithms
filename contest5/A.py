b950b3b1c72ded18a2914061188f2bf6026f01fb
berthakim <kte.ent@gmail.com>
2021-07-03 19:59:32 +0900

    Added some comments

--- contest5/A.py
+++ contest5/A.py
@@ -12,8 +12,8 @@ def find_min_diff_btwn_shortspents(shorts, pents):
             minshorts = shorts[s]
             minpents = pents[p]
 
-        if minval == 0:
-        	return minshorts, minpents
+        # if minval == 0:
+        #     return minshorts, minpents
 
         if shorts[s] > pents[p]:
             p += 1
@@ -33,3 +33,31 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+"""
+5
+1 2 3 4 100000
+10
+100 101 102 103 104 105 106 107 108 99949
+
+# shorts = [3, 4]
+# pents = [1, 2, 3]  # 3 3
+
+# shorts = [4, 5]
+# pents = [1, 2, 3]  # 4 3
+
+# shorts = [1, 2, 3, 4, 5]
+# pents = [1, 2, 3, 4, 5]  # 1 1
+"""
+
+"""
+About commented lines:
+if minval == 0:
+    return minshorts, minpents
+ Эта проверка в худшем случае замедлит ход программы,
+ так как на каждом шаге цикла будет выполнятся проверка.
+ Но в среднем это может дать выгоду и единственный способ
+ узнать будет ли это полезно или нет - запустить на данных,
+ похожих на реальные. в учебных задачах худший случай обязательно
+ будет
+ """
