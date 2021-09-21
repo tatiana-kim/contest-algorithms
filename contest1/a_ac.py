c0cf2c6375684f9be21121fa98ebc983282f521c
tatiana-kim <kte.ent@gmail.com>
2021-09-20 23:02:40 -0800

    Removed the commented-out code

--- contest1/a_ac.py
+++ contest1/a_ac.py
@@ -1,13 +1,3 @@
-# troom, tcond = input().split()
-# troom, tcond = int(troom), int(tcond)
-# while (troom < -50 or troom > 50) and (tcond < -50 or tcond > 50):
-#     troom, tcond = input().split()
-#     troom, tcond = int(troom), int(tcond)
-
-# mode = input()
-# while mode not in ("heat", "freeze", "auto", "fun"):
-#     print('Available options are: "heat", "freeze", "auto", "fun"')
-#     mode = input()
 troom, tcond = map(int, input().split())
 mode = input()
 
